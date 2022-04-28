dc688221b702b73995171861fe97c06264814a2c
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-04-28 12:30:33 -0400

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     return (
         <div className="expense-item">
-            <div>April 28 2022</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
+                <h2>{props.title}</h2>
             </div>
-            <div className="expense-item__price">$157.95</div>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     );
 }
